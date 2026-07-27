f07f74e4f453e3d98a31c7187a6ebfec1f31aecd
Aayush Kumar <aayushkumar4917@gmail.com>
2026-07-27 12:53:27 +0530

    Js Fundamentals

---
+++ JavaScript/Q6.js
@@ -0,0 +1,18 @@
+let string ="aayush kumar"
+let string2 = "mahesh"
+console.log(string + string2)
+console.log(string.length)
+
+let str = 3948
+console.log(`,adfasdf ${str}`) //string literal
+
+let str1 = new String("aayush kumar")
+
+let palin = "aayush"
+let check = palin.split("").reverse().join("")
+console.log(check)
+
+console.log(string.includes('h'))
+console.log(string.slice(3,7))
+console.log(string.toUpperCase())
+console.log(string.replace('kumar' , 'singh'))
